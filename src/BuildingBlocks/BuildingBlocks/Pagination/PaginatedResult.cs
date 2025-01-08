bffc57e0624f27397a110fd7b0381691b62113cf
NkeremBora <190601030@bakircay.edu.tr>
2025-01-08 23:08:05 +0300

    feat: add pagination

---
+++ src/BuildingBlocks/BuildingBlocks/Pagination/PaginatedResult.cs
@@ -0,0 +1,8 @@
+namespace BuildingBlocks.Pagination;
+
+public record PaginatedResult<TEntity>(
+    int PageIndex,
+    int PageSize,
+    long Count,
+    IEnumerable<TEntity> Data
+) where TEntity : class;
