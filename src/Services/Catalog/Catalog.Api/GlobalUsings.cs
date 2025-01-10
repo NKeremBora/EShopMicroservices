7f64295548789ade367d45422f9e2c36e9de618b
NkeremBora <190601030@bakircay.edu.tr>
2025-01-11 01:16:56 +0300

    feat: initialize Catalog.Api service

---
+++ src/Services/Catalog/Catalog.Api/GlobalUsings.cs
@@ -0,0 +1,12 @@
+global using Carter;
+global using Mapster;
+global using MediatR;
+global using Marten;
+global using FluentValidation;
+
+
+global using BuildingBlocks.CQRS;
+
+
+global using Catalog.API.Exceptions;
+global using Catalog.Api.Models;
