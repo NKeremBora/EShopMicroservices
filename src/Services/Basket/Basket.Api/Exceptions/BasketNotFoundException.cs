3ab6b6633c4a9e6dc657cf4b8704c5252e58926f
NkeremBora <190601030@bakircay.edu.tr>
2025-01-19 22:31:29 +0300

    feat(Basket.Api): add BasketNotFoundException

---
+++ src/Services/Basket/Basket.Api/Exceptions/BasketNotFoundException.cs
@@ -0,0 +1,9 @@
+namespace Basket.Api.Exceptions;
+
+public class BasketNotFoundException : NotFoundException
+{
+    public BasketNotFoundException(string userName) : base("Basket", userName)
+    {
+        
+    }
+}
