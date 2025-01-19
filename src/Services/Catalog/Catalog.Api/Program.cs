9a0ae3cdbe4b8ce2502bc0a9b6db4f2e6c8c4593
NkeremBora <190601030@bakircay.edu.tr>
2025-01-19 20:31:00 +0300

    refactor(Catalog.Api): organize Program.cs with service extensions

--- src/Services/Catalog/Catalog.Api/Program.cs
+++ src/Services/Catalog/Catalog.Api/Program.cs
@@ -1,16 +1,12 @@
+using BuildingBlocks.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
 var assembly = typeof(Program).Assembly;
-builder.Services.AddMediatR(config =>
-{
-    config.RegisterServicesFromAssembly(assembly);
-    config.AddOpenBehavior(typeof(ValidationBehavior<,>));
-    config.AddOpenBehavior(typeof(LoggingBehavior<,>));
-});
+
+builder.Services.AddMediatRWithAssembly(assembly);
+builder.Services.AddCarterWithAssembly(assembly);
 builder.Services.AddValidatorsFromAssembly(assembly);
-
-builder.Services.AddCarter();
-
 builder.Services.AddMarten(opts =>
 {
     opts.Connection(builder.Configuration.GetConnectionString("Database")!);    
