a65dc1b5544fcb2c89f9d31f5278b4fe469f78c2
NkeremBora <190601030@bakircay.edu.tr>
2025-01-19 22:30:54 +0300

    feat(Basket.Api): add IBasketRepository and its implementation

---
+++ src/Services/Basket/Basket.Api/GlobalUsings.cs
@@ -0,0 +1,18 @@
+global using Carter;
+global using Mapster;
+global using MediatR;
+global using Marten;
+global using Marten.Pagination;
+global using FluentValidation;
+
+global using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+
+global using BuildingBlocks.CQRS;
+global using BuildingBlocks.Behaviors;
+global using BuildingBlocks.Exceptions.Handler;
+global using BuildingBlocks.Extensions;
+global using BuildingBlocks.Exceptions;
+
+global using Basket.Api.Models;
+global using Basket.Api.Exceptions;
+global using Basket.Api.Data;
--- src/Services/Basket/Basket.Api/Program.cs
+++ src/Services/Basket/Basket.Api/Program.cs
@@ -1,7 +1,22 @@
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddCarter();
 
+var assembly = typeof(Program).Assembly;
+
+builder.Services.AddMediatRWithAssembly(assembly);
+builder.Services.AddCarterWithAssembly(assembly);
+builder.Services.AddValidatorsFromAssembly(assembly);
+builder.Services.AddMarten(opts =>
+{
+    opts.Connection(builder.Configuration.GetConnectionString("Database")!); 
+    opts.Schema.For<ShoppingCart>().Identity(x => x.UserName);
+}).UseLightweightSessions();
+builder.Services.AddScoped<IBasketRepository, BasketRepository>();
+builder.Services.AddExceptionHandler<CustomExceptionHandler>();
 
 var app = builder.Build();
+app.MapCarter();
+app.UseExceptionHandler(options => { });
 
 await app.RunAsync();
