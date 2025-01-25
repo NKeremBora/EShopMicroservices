54eeecc39835def461dd9bce0c5c82536c1566fc
NkeremBora <190601030@bakircay.edu.tr>
2025-01-26 00:37:53 +0300

    feat(discount.grpc): add coupon model and configure EF DbContext

--- src/Services/Discount/Discount.Grpc/Program.cs
+++ src/Services/Discount/Discount.Grpc/Program.cs
@@ -1,8 +1,12 @@
+using Discount.Grpc.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddGrpc();
-
+builder.Services.AddDbContext<DiscountContext>(opts =>
+    opts.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
