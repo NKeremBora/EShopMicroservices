183d84c82762a0eddc312d6f7a8d8cf4eb089b47
NkeremBora <190601030@bakircay.edu.tr>
2025-02-09 16:28:40 +0300

    feat(ordering.application): Add Commands, Queries, Event Handlers, DI, and Global Usings

--- src/Services/Ordering/Ordering.Application/DependencyInjection.cs
+++ src/Services/Ordering/Ordering.Application/DependencyInjection.cs
@@ -1,13 +1,20 @@
+using System.Reflection;
+using BuildingBlocks.Behaviors;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace Ordering.Infrastructure;
-
+namespace Ordering.Application;
 public static class DependencyInjection
 {
-    public static IServiceCollection AddApplication(this IServiceCollection services)
+    public static IServiceCollection AddApplication
+        (this IServiceCollection services, IConfiguration configuration)
     {
-        
+        services.AddMediatR(config =>
+        {
+            config.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+            config.AddOpenBehavior(typeof(ValidationBehavior<,>));
+            config.AddOpenBehavior(typeof(LoggingBehavior<,>));
+        });
         
         
         return services;
